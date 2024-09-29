06910f6277c0d3b9a4724e03a465f3762edf60d3
xnought <dbertucci6@gatech.edu>
2024-09-29 15:35:38 -0400

    start of svg model

--- src/App.svelte
+++ src/App.svelte
@@ -19,6 +19,7 @@
 	import Pairwise from "./lib/Pairwise.svelte";
 	import Argmin from "./lib/Argmin.svelte";
 	import SelectEmbed from "./lib/SelectEmbed.svelte";
+	import Sankey from "./lib/Sankey.svelte";
 
 	const inputOutputCanvasSize = 300;
 	const images = [1, 2, 3, 4, 5, 7].map((d) => `images/${d}.png`);
@@ -66,11 +67,115 @@
 	$: if (model && selectedImage) forward(rawImages[selectedImage]);
 	let featuresWidth = 150;
 	let featuresHeight = 300;
+
+	// svg positioning
+	$: inputX = 0;
+	$: inputEncoderGap = 30;
+
+	$: encoderX = inputX + inputOutputCanvasSize + inputEncoderGap;
+	$: encoderInHeight = inputOutputCanvasSize;
+	$: encoderOutHeight = inputOutputCanvasSize / 2;
+	$: encoderWidth = 100;
+	$: encoderOutX = encoderX + encoderWidth;
+	$: encoderOutY = inputOutputCanvasSize / 4;
+	$: middleGap = 300;
+
+	$: decoderX = middleGap + encoderOutX;
+	$: decoderY = encoderOutY;
+	$: decoderInHeight = encoderOutHeight;
+	$: decoderOutHeight = encoderInHeight;
+	$: decoderOutX = encoderWidth + decoderX;
+
+	$: outputX = decoderOutX + inputEncoderGap;
+
+	const sankeyColors = {
+		stroke: "rgba(255,255,255,0.3)",
+		fill: "rgba(255,255,255,0.05)",
+	};
 </script>
 
 <Header />
 <main class="p-5">
 	<div class="mb-2 flex gap-2 items-center">
+		<ImageSelector imageUrls={images} bind:selectedUrl={selectedImage} />
+	</div>
+	<svg>
+		<foreignObject
+			id="input"
+			x={inputX}
+			y={0}
+			width={inputOutputCanvasSize}
+			height={inputOutputCanvasSize + 100}
+		>
+			<MnistDigit
+				data={inputDigit}
+				square={inputOutputCanvasSize}
+				maxVal={1}
+				enableDrawing
+				onChange={(d) => {
+					forward(d);
+				}}
+			></MnistDigit>
+			<Button
+				class="mt-2"
+				size="xs"
+				color="alternative"
+				on:click={() => {
+					selectedImage = "clear";
+					rawImages = rawImages; // weirdly needed for UI to update;
+				}}><TrashBinOutline class="mr-1" size="sm" /> Clear</Button
+			>
+		</foreignObject>
+
+		<g id="encoder">
+			<text
+				x={encoderX + encoderWidth / 2}
+				y={inputOutputCanvasSize / 2}
+				text-anchor="middle"
+				style="fill: {sankeyColors.stroke}"
+				class="code">Encoder</text
+			>
+			<Sankey
+				p1={[encoderX, 0]}
+				p1Height={encoderInHeight}
+				p2={[encoderOutX, encoderOutY]}
+				p2Height={encoderOutHeight}
+				{...sankeyColors}
+			/>
+		</g>
+
+		<g id="decoder">
+			<text
+				x={decoderX + encoderWidth / 2}
+				y={inputOutputCanvasSize / 2}
+				style="fill: {sankeyColors.stroke}"
+				text-anchor="middle"
+				class="code">Decoder</text
+			>
+			<Sankey
+				p1={[decoderX, decoderY]}
+				p1Height={decoderInHeight}
+				p2={[decoderOutX, 0]}
+				p2Height={decoderOutHeight}
+				{...sankeyColors}
+			/>
+		</g>
+		<foreignObject
+			id="output"
+			x={outputX}
+			y={0}
+			width={inputOutputCanvasSize}
+			height={inputOutputCanvasSize + 100}
+		>
+			<MnistDigit
+				data={outputDigit}
+				square={inputOutputCanvasSize}
+				maxVal={1}
+			></MnistDigit>
+		</foreignObject>
+	</svg>
+
+	<!-- <div class="mb-2 flex gap-2 items-center">
 		<ImageSelector imageUrls={images} bind:selectedUrl={selectedImage} />
 	</div>
 	<div class="flex gap-5" id="tool">
@@ -166,7 +271,12 @@
 				maxVal={1}
 			></MnistDigit>
 		</div>
-	</div>
+	</div> -->
 </main>
 
-<style></style>
+<style>
+	svg,
+	foreignObject {
+		overflow: visible;
+	}
+</style>
